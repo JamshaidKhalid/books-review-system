29084eb63877b5aa3b7d33b7c107271bf271d08d
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2024-07-02 16:54:03 +0500

    logout api

--- bookreviewplatform/customer/urls.py
+++ bookreviewplatform/customer/urls.py
@@ -5,9 +5,11 @@ from rest_framework_simplejwt.views import (
 )
 
 from customer.views import RegisterCustomerAPIView
+from customer.views import LogoutAPIView
 
 urlpatterns = [
     path('api/login/', TokenObtainPairView.as_view(), name='login'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', RegisterCustomerAPIView.as_view(), name='register'),
+    path('api/logout/', LogoutAPIView.as_view(), name='logout'),
 ]
--- bookreviewplatform/customer/views.py
+++ bookreviewplatform/customer/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
 from customer.serializers import CustomerRegisterSerializer
 
 class RegisterCustomerAPIView(APIView):
@@ -11,7 +14,19 @@ class RegisterCustomerAPIView(APIView):
     def post(self, request, format=None):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            customer = serializer.save()
             serialized_data = serializer.data
-            return Response(serialized_data = serialized_data, status=201)
+            return Response(serialized_data, status=201)
         return Response(serializer.errors, status=400)
+
+
+class LogoutAPIView(APIView):
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh_token")
+            print(refresh_token)
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({'success': 'User logged out successfully'}, status=200)
+        except Exception as e:
+            return Response({'error': 'Something went wrong'}, status=400)
